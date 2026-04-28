e71fcedb74be3a985ad539faef13c3ecad8cee89
abdullah-full-stack-dev <khannnabdullah7750@gmail.com>
2026-04-28 14:28:09 +0530

    removed env from git

--- Backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-let express = require("express");
-const userAuth = require("../middleware/userAuth");
-const getUserData = require("../controller/userController");
-let userRouter = express.Router();
-
-userRouter.get("/data", userAuth, getUserData);
-
-module.exports = userRouter;
